6e491b4011d1052283bb21a8d8eb4d3ac2981c03
human-one1 <s1101756@gm.pu.edu.tw>
2023-05-31 09:51:13 +0800

    重新提交v.1

--- app/src/main/java/tw/edu/pu/csim/zuying/example/a/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/zuying/example/a/MainActivity.kt
@@ -3,12 +3,14 @@ package tw.edu.pu.csim.zuying.example.a
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.zuying.example.a.ui.theme.資管二A陳咨穎Theme
@@ -32,6 +34,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopStart
+    ) {
+        Text("Hello Android!")
+    }
     LazyRow {
 
         }
