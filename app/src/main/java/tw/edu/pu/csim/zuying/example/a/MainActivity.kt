3e5d5212fd6fb77c2b4962d5eda66309fa11dc11
human-one1 <s1101756@gm.pu.edu.tw>
2023-05-31 09:40:48 +0800

    提交v.1

---
+++ app/src/main/java/tw/edu/pu/csim/zuying/example/a/MainActivity.kt
@@ -0,0 +1,47 @@
+package tw.edu.pu.csim.zuying.example.a
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tw.edu.pu.csim.zuying.example.a.ui.theme.資管二A陳咨穎Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            資管二A陳咨穎Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    LazyRow {
+
+        }
+
+    }
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    資管二A陳咨穎Theme {
+        Greeting("Android")
+    }
+}
