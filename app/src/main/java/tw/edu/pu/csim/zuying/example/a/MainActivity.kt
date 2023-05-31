f0979d48f2b6c6c5fe664077458035c2ce4bc06e
human-one1 <s1101756@gm.pu.edu.tw>
2023-05-31 10:13:38 +0800

    提交v.2

--- app/src/main/java/tw/edu/pu/csim/zuying/example/a/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/zuying/example/a/MainActivity.kt
@@ -3,8 +3,11 @@ package tw.edu.pu.csim.zuying.example.a
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,7 +15,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import tw.edu.pu.csim.zuying.example.a.ui.theme.資管二A陳咨穎Theme
 
 class MainActivity : ComponentActivity() {
@@ -34,17 +40,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.TopStart
-    ) {
-        Text("Hello Android!")
-    }
-    LazyRow {
 
+    Column{
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Text("作者:資管二A 陳咨穎")
+            Image(
+                painter = painterResource(R.drawable.map),
+                contentDescription = "圖片描述",
+                modifier = Modifier.size(500.dp)
+            )
         }
 
     }
+
+
+}
 
 @Preview(showBackground = true)
 @Composable
